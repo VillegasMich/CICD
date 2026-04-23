aa92b1320f6f84b66d2df816eeb7e0ccd0d8539c
mvasquezb <mvasquezb@growthaccelerationpartners.com>
2026-04-22 22:27:15 -0500

    feat: changed cypress API URL in pipe

--- frontend/e2e/auth.cy.js
+++ frontend/e2e/auth.cy.js
@@ -5,7 +5,7 @@ const TEST_USER = {
 };
 
 before(() => {
-  cy.request({ method: "POST", url: "/api/v1/auth/register", body: TEST_USER, failOnStatusCode: false });
+  cy.request({ method: "POST", url: `${Cypress.env("API_URL")}/api/v1/auth/register`, body: TEST_USER, failOnStatusCode: false });
 });
 
 describe("Authentication", () => {
